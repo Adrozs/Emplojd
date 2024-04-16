251f1eb2b93dd7532c917923d7753608b21d86d4
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-04-16 15:48:57 +0200

    ApplyNow sidan

--- src/pages/ApplyNow/ApplyNow.jsx
+++ src/pages/ApplyNow/ApplyNow.jsx
@@ -46,58 +46,138 @@ function ApplyNow() {
           <div className="mt-4 text-center font-semibold">
             <h2 className="text-2xl">Generera ditt personliga brev</h2>
             <div className=" m-4 flex items-center justify-center gap-4">
-              <span className="h-4 w-4 bg-slate-600 rounded-full"></span>
-              <span className="h-4 w-4 bg-slate-300 rounded-full"></span>
-              <span className="h-4 w-4 bg-slate-300 rounded-full"></span>
+              <span
+                className={
+                  page === 1
+                    ? "h-4 w-4 bg-slate-600 rounded-full"
+                    : "h-4 w-4 rounded-full  bg-slate-300"
+                }
+              ></span>
+              <span
+                className={
+                  page === 2
+                    ? "h-4 w-4 bg-slate-600 rounded-full"
+                    : "h-4 w-4 rounded-full  bg-slate-300"
+                }
+              ></span>
+              <span
+                className={
+                  page === 3
+                    ? "h-4 w-4 bg-slate-600 rounded-full"
+                    : "h-4 w-4 rounded-full bg-slate-300"
+                }
+              ></span>
             </div>
             <h2 className="mt-4 text-2xl font-semibold">{page}.</h2>
           </div>
         )}
       </div>
-      <ul className="flex justify-center">
-        <li className="h-[260px] w-[90%] bg-stone-200 p-4 flex flex-col justify-between">
-          <div>
-            <div className=" flex items-center justify-center p-1 w-[100%]]">
-              <p>Jobb du söker</p>
-            </div>
-          </div>
-          <div>
-            <h3 className="font-semibold text-xl text-stone-900">
-              {job.headline}
-            </h3>
-            <p className="text-lg">{job.employer.name}</p>
-            <div>
-              <p className="text-sm my-2">
-                {job.workplace_address.municipality} -{" "}
-                {daysUntilDeadline > 0
-                  ? daysUntilDeadline === 1
-                    ? "1 dag kvar till sista ansökningsdagen"
-                    : `${daysUntilDeadline} dagar kvar till sista ansökningsdagen`
-                  : "Sista ansökningsdagen har passerat"}
-              </p>
-            </div>
-          </div>
-          <div className="flex gap-2 text-[12px]">
-            {job.working_hours_type.label ? (
-              <span className="bg-stone-100 px-2 py-1">
-                {job.working_hours_type.label}
-              </span>
-            ) : (
-              <span className="bg-stone-100 px-2 py-1">
-                {job.employment_type.label}{" "}
-              </span>
-            )}
+      {page === 1 ? (
+        <>
+          <ul className="flex justify-center">
+            <li className="h-[260px] w-[90%] bg-stone-200 p-4 flex flex-col justify-between">
+              <div>
+                <div className=" flex items-center justify-center p-1 w-[100%]]">
+                  <p>Jobb du söker</p>
+                </div>
+              </div>
+              <div>
+                <h3 className="font-semibold text-xl text-stone-900">
+                  {job.headline}
+                </h3>
+                <p className="text-lg">{job.employer.name}</p>
+                <div>
+                  <p className="text-sm my-2">
+                    {job.workplace_address.municipality} -{" "}
+                    {daysUntilDeadline > 0
+                      ? daysUntilDeadline === 1
+                        ? "1 dag kvar till sista ansökningsdagen"
+                        : `${daysUntilDeadline} dagar kvar till sista ansökningsdagen`
+                      : "Sista ansökningsdagen har passerat"}
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-2 text-[12px]">
+                {job.working_hours_type.label ? (
+                  <span className="bg-stone-100 px-2 py-1">
+                    {job.working_hours_type.label}
+                  </span>
+                ) : (
+                  <span className="bg-stone-100 px-2 py-1">
+                    {job.employment_type.label}{" "}
+                  </span>
+                )}
 
-            <span className="bg-stone-100 px-2 py-1">
-              {job.occupation.label}{" "}
-            </span>
+                <span className="bg-stone-100 px-2 py-1">
+                  {job.occupation.label}{" "}
+                </span>
+              </div>
+              <div className="flex justify-between">
+                <button onClick={handleBack}>Avbryt</button>
+                <button className="bg-stone-100 p-1" onClick={() => setPage(2)}>
+                  Nästa
+                </button>
+              </div>
+            </li>
+          </ul>
+        </>
+      ) : (
+        <ApplySideTwo job={job} page={page} setPage={setPage} />
+      )}
+    </main>
+  );
+}
+
+function ApplySideTwo({ job, page, setPage }) {
+  return (
+    <main className="flex justify-center">
+      <div className=" w-[90%] bg-stone-200 p-4 flex flex-col pb-10">
+        <div className="flex flex-col items-center text-center">
+          <p>Vad ska vara med i brevet?</p>
+          <div className="flex gap-3 my-10">
+            <p>Jobb</p>
+            <div className="w-[100px] bg-slate-300 h-5">
+              <span className=" w-[20px] h-5 bg-slate-600"></span>
+            </div>
+            <p>Praktik</p>
           </div>
-          <div className="flex justify-between">
-            <button onClick={handleBack}>Avbryt</button>
-            <button className="bg-stone-100 p-1">Nästa</button>
-          </div>
-        </li>
-      </ul>
+        </div>
+        <div>
+          <p>Arbetstitle</p>
+          <p className="text-xl font-semibold">{job.headline}</p>
+        </div>
+        <form className="max-w-sm my-4">
+          <label className="flex flex-col text-xl mb-4">
+            <span>Namn</span>
+            <input
+              className="p-1"
+              type="text"
+              placeholder="Namn Efternamnsson"
+            />
+          </label>
+          <label className="flex flex-col text-xl my-4">
+            <span>Intressen</span>
+            <input className="p-1 mt-1" type="text" placeholder="..." />
+          </label>
+          <label className="flex flex-col text-xl my-4">
+            <span>Intressen</span>
+            <input className="p-1 mt-1" type="file" placeholder="CV-namn.pdf" />
+          </label>
+          <label className="flex flex-col text-xl my-4">
+            <span>Beskrivande ord (t.ex. snabblärd, per)</span>
+            <input className="p-1 mt-1" type="text" placeholder="CV-namn.pdf" />
+          </label>
+        </form>
+        <div className="flex justify-between">
+          <button onClick={() => setPage(1)}>Tillbaka</button>
+          <button
+            className="bg-stone-100 text-xl p-1 rounded-full"
+            onClick={() => setPage(3)}
+          >
+            &rarr;
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
--- src/pages/JobList/JobSearchForm.jsx
+++ src/pages/JobList/JobSearchForm.jsx
@@ -82,7 +82,7 @@ function JobSearchForm() {
             value={job}
             ref={inputEl}
             onChange={(e) => setJob(e.target.value)}
-            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 dark:bg-gray-700 dark:shadow-sm-light"
             placeholder="...."
           />
         </div>
@@ -93,7 +93,7 @@ function JobSearchForm() {
           <select
             value={city}
             onChange={(e) => setCity(e.target.value)}
-            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 dark:bg-gray-700 dark:shadow-sm-light"
             placeholder="...."
           >
             <option value=" ">Hela landet</option>
