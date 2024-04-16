12c8a82f746331e595a61ad86a74cc4d5857534b
Andy <andycheung83a@hotmail.com>
2024-04-16 17:17:14 +0200

    add: Såhär går det till section on LandingPage

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import { BsArrowDownCircle } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import JobList from '../JobList/JobList';
 import JobSearchForm from '../JobList/JobSearchForm';
+import { LiaKissWinkHeartSolid } from 'react-icons/lia';
 
 export default function LandingPage() {
 	return (
@@ -38,27 +39,57 @@ export default function LandingPage() {
 			</div>
 			<JobSearchForm />
 			<div className="min-h-96 mt-10">
-				<h6 className="mt-2 flex-col font-semi-bold items-center justify-center text-center text-2xl">
+				<h6 className="mt-2 flex-col font-semi-bold items-center justify-center text-center text-2xl mb-10">
 					Såhär går det till
 				</h6>
-				<div>
-					<button className="p-5 px-6 pt-2 baseline">
-						<BsArrowDownCircle size="2rem" />
-					</button>
-					<h6 className="font-bold text-xl">1. lorem ipsum</h6>
-					<p>lorem ipsum lorem ipsum lorem </p>
+
+				<div className="flex mr-10 ms-10 mb-20">
+					<div>
+						<button className="p-5 px-6 pt-2 baseline">
+							<LiaKissWinkHeartSolid size="2rem" />
+						</button>
+						<h6 className="font-bold text-xl">1. lorem ipsum</h6>
+						<p>lorem ipsum lorem ipsum lorem </p>
+					</div>
+
+					<div>
+						<button className="p-5 px-6 pt-2 baseline">
+							<LiaKissWinkHeartSolid size="2rem" />
+						</button>
+						<h6 className="font-bold text-xl">1. lorem ipsum</h6>
+						<p>lorem ipsum lorem ipsum lorem </p>
+					</div>
 				</div>
-				<button className="p-5 px-6 pt-2 baseline">
-					<BsArrowDownCircle size="2rem" />
-				</button>
-				<h6 className="font-bold text-xl">1. lorem ipsum</h6>
-				<p>lorem ipsum lorem ipsum lorem </p>
-				<button className="p-5 px-6 pt-2 baseline">
-					<BsArrowDownCircle size="2rem" />
-				</button>
-				<h6 className="font-bold text-xl">1. lorem ipsum</h6>
-				<p>lorem ipsum lorem ipsum lorem </p>
+				<div className="flex mr-10 ms-10 min-h-52">
+					<div>
+						<button className="p-5 px-6 pt-2 baseline">
+							<LiaKissWinkHeartSolid size="2rem" />
+						</button>
+						<h6 className="font-bold text-xl">1. lorem ipsum</h6>
+						<p>lorem ipsum lorem ipsum lorem </p>
+					</div>
+
+					<div>
+						<button className="p-5 px-6 pt-2 baseline">
+							<LiaKissWinkHeartSolid size="2rem" />
+						</button>
+						<h6 className="font-bold text-xl">1. lorem ipsum</h6>
+						<p>lorem ipsum lorem ipsum lorem </p>
+					</div>
+				</div>
+				<div className="flex justify-center md:justify-center">
+					<Link to="/signin" className="p-3 px-40 font-bold bg-stone-300">
+						CTA?
+					</Link>
+				</div>
+				<h6 className="mt-2 font-semi-bold mt-20 ms-10 text-2xl">TEXT</h6>
 			</div>
+			<p className="ms-10 mr-10 mt-5 mb-10">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+				commodo consequat.
+			</p>
 		</>
 	);
 }
