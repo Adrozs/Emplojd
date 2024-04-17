3ea5fe0dacf5f37ae30c157c4763c639f528fb02
Andy <andycheung83a@hotmail.com>
2024-04-17 10:13:41 +0200

    Moved Footer to its own component
    I extracted the Footer into its own component file and into the folder components for better structure and maintainability.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,35 +1,57 @@
+import { AiFillTwitterCircle } from 'react-icons/ai';
+import { FaFacebook } from 'react-icons/fa';
+import { FaInstagramSquare } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 export default function Footer() {
 	return (
 		<>
-			<div className="bg-stone-400">
-				<h2>LOGO</h2>
-				<h3>TEXT</h3>
-				<p>Min sida</p>
-				<p>Sök jobb</p>
-				<h2>Följ oss på sociala medier</h2>
-				<button className="p-5 px-6 pt-2 baseline">
-					<LiaKissWinkHeartSolid size="2rem" />
-				</button>
-				<button className="p-5 px-6 pt-2 baseline">
-					<LiaKissWinkHeartSolid size="2rem" />
-				</button>
-				<button className="p-5 px-6 pt-2 baseline">
-					<LiaKissWinkHeartSolid size="2rem" />
-				</button>
-				<h3>Ladda ner vår app</h3>
-				<div className="flex justify-center md:justify-center">
-					<Link to="/signin" className="p-3 px-40 font-bold bg-stone-300">
-						CTA?
-					</Link>
+			<div>
+				<div className="bg-stone-400">
+					<div className="flex justify-center">
+						<h2 className="inline bg-slate-100 p-2.5 pl-5 pr-5 font-bold mt-10 mb-10 text-3xl">
+							LOGO
+						</h2>
+					</div>
+					<div className="text-center mb-10">
+						<h3 className="mb-5 font-bold text-xl">TEXT</h3>
+						<p className="mb-3">Min sida</p>
+						<p className="mb-3">Sök jobb</p>
+					</div>
+					<h2 className="flex justify-center font-bold mb-3 text-xl">
+						Följ oss på sociala medier
+					</h2>
+					<div className="flex justify-center">
+						<button className="p-5 px-6 pt-2 baseline">
+							<AiFillTwitterCircle size="2rem" />
+						</button>
+						<button className="p-5 px-6 pt-2 baseline">
+							<FaInstagramSquare size="2rem" />
+						</button>
+						<button className="p-5 px-6 pt-2 baseline">
+							<FaFacebook size="2rem" />
+						</button>
+					</div>
+					<h3 className="flex justify-center text-xl font-bold mb-7 mt-4">
+						Ladda ner vår app
+					</h3>
+					<div className="flex justify-center justify-evenly ">
+						<div className="">
+							<Link to="/signin" className="p-3 px-6 font-bold bg-stone-300">
+								App Store
+							</Link>
+						</div>
+						<div className="">
+							<Link to="/signin" className="p-3 px-6 font-bold bg-stone-300">
+								Google Play
+							</Link>
+						</div>
+					</div>
+					<p className="flex justify-center pt-12 pb-7 text-xs ">
+						Copyright bla bla bla
+					</p>
 				</div>
-				<div className="flex justify-center md:justify-center">
-					<Link to="/signin" className="p-3 px-40 font-bold bg-stone-300">
-						CTA?
-					</Link>
-				</div>
-				<p>Copyright bla bla bla</p>
 			</div>
-			;
 		</>
 	);
 }
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,8 @@
 import { BsArrowDownCircle } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
-import JobList from '../JobList/JobList';
 import JobSearchForm from '../JobList/JobSearchForm';
 import { LiaKissWinkHeartSolid } from 'react-icons/lia';
-import { AiFillTwitterCircle } from 'react-icons/ai';
-import { FaInstagramSquare } from 'react-icons/fa';
-import { FaFacebook } from 'react-icons/fa';
+import Footer from '../../components/Footer';
 
 export default function LandingPage() {
 	return (
@@ -93,52 +90,7 @@ export default function LandingPage() {
 				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
 				commodo consequat.
 			</p>
-			<div>
-				<div className="bg-stone-400">
-					<div className="flex justify-center">
-						<h2 className="inline bg-slate-100 p-2.5 pl-5 pr-5 font-bold mt-10 mb-10 text-3xl">
-							LOGO
-						</h2>
-					</div>
-					<div className="text-center mb-10">
-						<h3 className="mb-5 font-bold text-xl">TEXT</h3>
-						<p className="mb-3">Min sida</p>
-						<p className="mb-3">Sök jobb</p>
-					</div>
-					<h2 className="flex justify-center font-bold mb-3 text-xl">
-						Följ oss på sociala medier
-					</h2>
-					<div className="flex justify-center">
-						<button className="p-5 px-6 pt-2 baseline">
-							<AiFillTwitterCircle size="2rem" />
-						</button>
-						<button className="p-5 px-6 pt-2 baseline">
-							<FaInstagramSquare size="2rem" />
-						</button>
-						<button className="p-5 px-6 pt-2 baseline">
-							<FaFacebook size="2rem" />
-						</button>
-					</div>
-					<h3 className="flex justify-center text-xl font-bold mb-7 mt-4">
-						Ladda ner vår app
-					</h3>
-					<div className="flex justify-center justify-evenly ">
-						<div className="">
-							<Link to="/signin" className="p-3 px-6 font-bold bg-stone-300">
-								App Store
-							</Link>
-						</div>
-						<div className="">
-							<Link to="/signin" className="p-3 px-6 font-bold bg-stone-300">
-								Google Play
-							</Link>
-						</div>
-					</div>
-					<p className="flex justify-center pt-12 pb-7 text-xs ">
-						Copyright bla bla bla
-					</p>
-				</div>
-			</div>
+			<Footer />
 		</>
 	);
 }
