7737ad91ee8e5e49c43f83db825eb60103017bf1
adrozs <adrozsahegyi@gmail.com>
2024-04-17 10:27:27 +0200

    Added solution to project

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1 className="text-3xl font-bold underline">Hello ChasGPT!</h1>;
-}
-
-export default App;
