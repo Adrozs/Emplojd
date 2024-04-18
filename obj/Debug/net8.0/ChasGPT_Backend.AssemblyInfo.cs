fd9392bc347e4ed76ecd048fcc67ccec8c4f7a53
Adrozs <adrozsahegyi@gmail.com>
2024-04-18 21:17:10 +0200

    random files that popped up

--- obj/Debug/net8.0/ChasGPT_Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChasGPT_Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChasGPT_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a2c4a99e49a13c131012e83f87acf7b310cd4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChasGPT_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChasGPT_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
