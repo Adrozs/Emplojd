8fc49cb5a1df91d9350e39ce7b95605ff45228cc
Adrozs <adrozsahegyi@gmail.com>
2024-04-19 15:39:38 +0200

    Setup Identity to the program and preliminary database connection, altough that is commented away until we have settled on a database system and we have read up on how that works and should be connected.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 
+using ChasGPT_Backend.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace ChasGPT_Backend
 {
     public class Program
@@ -6,6 +10,16 @@ namespace ChasGPT_Backend
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Setup database context and connection string here
+            //builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddIdentity<User, IdentityRole>()
+                //.AddEntityFrameworkStores<ApplicationDbContext>()
+                 .AddDefaultTokenProviders();
+
+
 
             // Add services to the container.
             builder.Services.AddAuthorization();
