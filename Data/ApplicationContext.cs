b12f11a9ad1a99c85b7a50997fb6b2c2e642202b
Adrozs <adrozsahegyi@gmail.com>
2024-04-26 10:46:47 +0200

    Created app context to connect to the db

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using ChasGPT_Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChasGPT_Backend.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+    }
+}
