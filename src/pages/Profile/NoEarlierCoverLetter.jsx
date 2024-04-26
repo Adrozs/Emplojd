d0a27644654042fd2497249ee63c943a1dfd2200
Andy <andycheung83a@hotmail.com>
2024-04-26 13:09:36 +0200

    Refactor styling in NoEarlierCoverLetter component and update text

--- src/pages/Profile/NoEarlierCoverLetter.jsx
+++ src/pages/Profile/NoEarlierCoverLetter.jsx
@@ -5,10 +5,8 @@ import { FaLongArrowAltRight } from 'react-icons/fa';
 function NoEarlierCoverLetter() {
 	return (
 		<>
-			<div className="bg-stone-300 flex justify-center items-center mb-6">
-				<h6 className="font-semi-bold text-xl text-center">
-					MITT PERSONLIGA BREV
-				</h6>
+			<div className="bg-stone-300 flex justify-center p-3 items-center mt-10 mb-6 ml-10 mr-10">
+				<h6 className="font-bold text-xl text-center">MITT PERSONLIGA BREV</h6>
 			</div>
 
 			<div className="bg-stone-300 flex  mb-6">
@@ -17,9 +15,9 @@ function NoEarlierCoverLetter() {
 				</p>
 			</div>
 
-			<div className="flex justify-around p-1 text-xs mt-3 mr-6">
-				<p className="bg-stone-300 p-1 text-center pl-3 pr-3">FÖRETAG</p>
-				<p className="bg-stone-300 p-1 text-center pl-3 pr-3">FÖRETAG</p>
+			<div className="flex justify-around text-xs mt-3 mr-6">
+				<p className="bg-stone-300 p-20 text-center pl-3 pr-3">FÖRETAG</p>
+				<p className="bg-stone-300 p-20 text-center pl-3 pr-3">FÖRETAG</p>
 			</div>
 
 			<div className="flex justify-around  p-1 text-xs mt-3 mr-6">
