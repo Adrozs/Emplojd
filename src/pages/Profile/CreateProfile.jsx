2fa3ef48f1823f6c71df314c16de097bf25de14f
Simon-jmk <151679315+Simon-jmk@users.noreply.github.com>
2024-04-30 15:18:28 +0200

    createProfile trashSVg

--- src/pages/Profile/CreateProfile.jsx
+++ src/pages/Profile/CreateProfile.jsx
@@ -87,6 +87,19 @@ function CreateProfile() {
       />
     </svg>
   );
+  const DeleteSVG = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1.5em"
+      height="1.5em"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        d="M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6zM8 9h8v10H8zm7.5-5l-1-1h-5l-1 1H5v2h14V4z"
+      />
+    </svg>
+  );
 
   return (
     <>
@@ -136,7 +149,7 @@ function CreateProfile() {
                           className="ml-auto p-2"
                           onClick={handleDeleteFile}
                         >
-                          x
+                          <DeleteSVG />
                         </button>
                       )}
                     </div>
