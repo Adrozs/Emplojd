302ec47159eccf8945eebd4d5196456130ae253e
Adrozs <adrozsahegyi@gmail.com>
2024-05-01 21:17:30 +0200

    Aded JWT setup and changed method names.
    -Added authentication setup so the system always checks for a token when recieving API calls (unless endpoint specified that it doesn't need authentication).  And what info the bearer should consist of (issuer, audienmce and secret).
    
    -Also changed login endpoint to a post instead of a get for security.
    
    -Changed name of login and added async to all async method names

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using ChasGPT_Backend.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ChasGPT_Backend.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ChasGPT_Backend
 {
@@ -13,6 +15,7 @@ namespace ChasGPT_Backend
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            ConfigurationManager configuration = builder.Configuration;
 
             // Setup database context and connection string here
             builder.Services.AddDbContext<ApplicationContext>(options =>
@@ -30,10 +33,33 @@ namespace ChasGPT_Backend
             .AddEntityFrameworkStores<ApplicationContext>() // Connects identity to the database giving its method ability to access it
             .AddDefaultTokenProviders();
 
+            // Adding authentication
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            // Add JWT Bearer
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true; // Allows the server to save the token for the duration of the request
+                options.RequireHttpsMetadata = true; // Enforces HTTPS so tokens aren't transfered over unsecure connections
+                options.TokenValidationParameters = new TokenValidationParameters() // The rules of which authorization will check
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = configuration["JWT:Issuer"],
+                    ValidAudience = configuration["JWT:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration["JWT:Secret"])) // Symetric key lets the system know the same secret is used for both signing and verifying the JWT. Then encodes it into bytes
+                };
+            });
+
+
+
             // Add repositories to scope
             builder.Services.AddScoped<IUserRepository, UserRepository>();
             builder.Services.AddScoped<IJobAdRepository, JobAdRepository>();
-
 
 
             // Add services to the container.
@@ -54,15 +80,16 @@ namespace ChasGPT_Backend
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication(); // Forces all api calls to use the JWT (token) to be authorized. (Unless specified).
             app.UseAuthorization();
 
 
             // Endpoints 
 
             // User account 
-            app.MapGet("/login/{email}/{password}", UserService.VerifyLogin);
-            app.MapPost("/create-account/{email}/{password}/{passwordConfirm}", UserService.CreateAccount);
-            app.MapPost("/change-password/{email}/{password}/{newPassword}/{newPasswordConfirm}", UserService.ChangePassword);
+            app.MapPost("/login/{email}/{password}", UserService.LoginAsync);
+            app.MapPost("/create-account/{email}/{password}/{passwordConfirm}", UserService.CreateAccountAsync);
+            app.MapPost("/change-password/{email}/{password}/{newPassword}/{newPasswordConfirm}", UserService.ChangePasswordAsync);
 
             // Job search
             app.MapGet("/search/{search}/{region?}/{offset?}", JobAdService.SearchJob);
