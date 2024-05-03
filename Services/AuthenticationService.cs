f71e60e7bc493894e6c5f6e181e07110e048d3c2
Adrozs <adrozsahegyi@gmail.com>
2024-05-03 11:16:20 +0200

    Created authentication service to separate logic and to allow for scalability, testing. Dependency injection.

---
+++ Services/AuthenticationService.cs
@@ -0,0 +1,21 @@
+﻿using ChasGPT_Backend.Models;
+using ChasGPT_Backend.Repositories;
+
+namespace ChasGPT_Backend.Services
+{
+    public class AuthenticationService
+    {
+        private readonly JwtRepository _jwtRepository;
+
+        public AuthenticationService(JwtRepository jwtRepository)
+        {
+            _jwtRepository = jwtRepository;
+        }
+
+        // Call the generate jwt method
+        public string GenerateToken(User user)
+        {
+            return _jwtRepository.GenerateJwt(user);
+        }
+    }
+}
