bfc0d8fd7da2b13007e0fcdedd6cc22cb6d01d00
Adrozs <adrozsahegyi@gmail.com>
2024-05-03 11:23:01 +0200

    Added CORS, fixed typos,  added to scope & changed all the endpoints.
    -Changed endpoints so all account enpoints now take in the information in the body instead of in the query.
    
    The job search endpoint now is modular and flexible allowing for omitting of parameters (except search). So you don't have to write all the parameters you don't wanna use like page. it'll then just get the default value.
    
    -Added cors to allow for usage of the api from any source, and without https as i'm using local host for testing. We should remove or configure this before deploying to production.
    
    -Fixed typos where TokenValidationParameters was missing parenthases and I had spelled "Jwt" with all caps "JWT" meaning it did not match the appsettings.
    
    -Added JwtRepository to scope as a singleton as it should only be one instance of it across the program.
    
    -Added AuthenticationService to scope as wel

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using ChasGPT_Backend.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ChasGPT_Backend
 {
@@ -22,16 +23,25 @@ namespace ChasGPT_Backend
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             // Adding Microsoft identity with config settings
-            builder.Services.AddIdentity<User, IdentityRole>(options => // IdentityUser instead??
+            builder.Services.AddIdentity<User, IdentityRole>(options =>
             {
                 options.Password.RequiredLength = 8;
                 options.Password.RequireLowercase = true;
-                options.Password.RequireUppercase = true; 
-                options.Password.RequireDigit = true; 
-                options.Password.RequireNonAlphanumeric = true; 
+                options.Password.RequireUppercase = true;
+                options.Password.RequireDigit = true;
+                options.Password.RequireNonAlphanumeric = true;
             })
             .AddEntityFrameworkStores<ApplicationContext>() // Connects identity to the database giving its method ability to access it
             .AddDefaultTokenProviders();
+
+            // Add CORS services and define a very permissive policy (CHANGE BEFORE PRODUCTION - ONLY FOR TESTING!)
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("OpenCorsPolicy", builder =>
+                    builder.AllowAnyOrigin()  // Allows requests from any source
+                           .AllowAnyMethod()  // Allows all HTTP methods
+                           .AllowAnyHeader()); // Allows any header
+            });
 
             // Adding authentication
             builder.Services.AddAuthentication(options =>
@@ -45,21 +55,26 @@ namespace ChasGPT_Backend
             {
                 options.SaveToken = true; // Allows the server to save the token for the duration of the request
                 options.RequireHttpsMetadata = true; // Enforces HTTPS so tokens aren't transfered over unsecure connections
-                options.TokenValidationParameters = new TokenValidationParameters() // The rules of which authorization will check
+                options.TokenValidationParameters = new TokenValidationParameters // The rules of which authorization will check
                 {
                     ValidateIssuer = true,
                     ValidateAudience = true,
-                    ValidIssuer = configuration["JWT:Issuer"],
-                    ValidAudience = configuration["JWT:Audience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration["JWT:Secret"])) // Symetric key lets the system know the same secret is used for both signing and verifying the JWT. Then encodes it into bytes
+                    ValidateLifetime = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration["Jwt:Secret"])) // Symetric key lets the system know the same secret is used for both signing and verifying the JWT. Then encodes it into bytes
                 };
             });
 
 
 
-            // Add repositories to scope
+            // Add to scope
             builder.Services.AddScoped<IUserRepository, UserRepository>();
             builder.Services.AddScoped<IJobAdRepository, JobAdRepository>();
+            builder.Services.AddSingleton<JwtRepository>(provider =>
+                new JwtRepository(provider.GetRequiredService<IConfiguration>()));
+            builder.Services.AddScoped<AuthenticationService>();
+
 
 
             // Add services to the container.
@@ -80,20 +95,33 @@ namespace ChasGPT_Backend
 
             app.UseHttpsRedirection();
 
-            app.UseAuthentication(); // Forces all api calls to use the JWT (token) to be authorized. (Unless specified).
+            // Apply the CORS policy - REMOVE AFTER TESTING 
+            app.UseCors("OpenCorsPolicy");
+
+            // Forces all api calls to use the JWT (token) to be authorized. (Unless specified).
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
-            // Endpoints 
+
+            // Endpoints
+            // Note: .AllowAnonymous means specified to allow access without token - use with caution.
 
             // User account 
-            app.MapPost("/login/{email}/{password}", UserService.LoginAsync);
-            app.MapPost("/create-account/{email}/{password}/{passwordConfirm}", UserService.CreateAccountAsync);
-            app.MapPost("/change-password/{email}/{password}/{newPassword}/{newPasswordConfirm}", UserService.ChangePasswordAsync);
+            app.MapPost("/login", UserService.LoginAsync).AllowAnonymous();
+            app.MapPost("/create-account", UserService.CreateAccountAsync).AllowAnonymous();
+            app.MapPost("/change-password", UserService.ChangePasswordAsync);
+
 
             // Job search
-            app.MapGet("/search/{search}/{region?}/{offset?}", JobAdService.SearchJob);
-            app.MapGet("/ad/{jobId}", JobAdService.GetJobFromId);
+
+            // Made the URI flexible to be able to omit parameters that aren't search from the query
+            app.MapGet("/search", async (string query, int? region, int? page, IJobAdRepository jobAdRepository) =>
+            {
+                return await JobAdService.SearchJob(query, region, page ?? 1, jobAdRepository);
+            });
+
+            app.MapGet("/ad/{adId}", JobAdService.GetJobFromId);
 
 
             app.Run();
