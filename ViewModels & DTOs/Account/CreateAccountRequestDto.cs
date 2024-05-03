0adc05302e214f4eb921091dc326a4f0542dea80
Adrozs <adrozsahegyi@gmail.com>
2024-05-03 10:56:18 +0200

    Created DTOs for the login endpoints
    Used to send in all information in the body instead of in the query

---
+++ ViewModels & DTOs/Account/CreateAccountRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ChasGPT_Backend.ViewModels___DTOs
+{
+    public class CreateAccountRequestDto
+    {
+        public string Email { get; set; }
+        public string EmailConfirmed { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirmed { get; set; }
+    }
+}
