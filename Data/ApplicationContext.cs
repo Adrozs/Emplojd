070807a4e303f8c4b90b6e5b4adfe3cdb2a73442
Fady Hatta <Fadynhatta@gmail.com>
2024-05-05 18:45:13 +0200

    Updated the ApplicationContext with the new Models

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -6,7 +6,11 @@ namespace ChasGPT_Backend.Data
     public class ApplicationContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<CoverLetter> CoverLetters { get; set; }
+        public DbSet<SavedJobAds> SavedJobAds { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
 
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
     }
 }
