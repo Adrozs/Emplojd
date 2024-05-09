406549240af71228e968df8c5445769094beb8f1
Adrozs <adrozsahegyi@gmail.com>
2024-05-09 16:32:40 +0200

    Added DTO used to take input from the ednpoint

---
+++ ViewModels & DTOs/Account/ResetPasswordRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ChasGPT_Backend.ViewModels___DTOs.Account
+{
+    public class ResetPasswordRequest
+    {
+        public string NewPassword { get; set; }
+        public string NewPasswordConfirm { get; set; }
+    }
+}
