a0b9607218a225c32395f7801f82a0ed055dbbc1
Adrozs <adrozsahegyi@gmail.com>
2024-05-09 20:28:09 +0200

    Added relationship between User and SavedJobAds

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -9,5 +9,16 @@ namespace ChasGPT_Backend.Data
         public DbSet<User> Users { get; set; }
 
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        // User and SavedJobAds relationship
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasMany(p => p.SavedJobAds)
+                .WithMany(j => j.Users)
+                .UsingEntity(j => j.ToTable("UserJobAds"));
+        }
     }
 }
