0230b9d9e943e59dc0d66a3575975803309d45a1
Adrozs <adrozsahegyi@gmail.com>
2024-05-09 20:36:48 +0200

    Added DTOs to handle the data transfer of retrieving, saving and deleting job ads

---
+++ ViewModels & DTOs/JobAds/SaveJobAdRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ChasGPT_Backend.ViewModels___DTOs.JobAds
+{
+    public class SaveJobAdRequest
+    {
+        public int PlatsbankenJobId { get; set; }
+        public string Headline { get; set; }
+        public string Employer { get; set; }
+    }
+}
