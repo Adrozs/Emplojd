2ad1fe15aa4370566f0c32788b45d9f88ee754c5
Adrozs <adrozsahegyi@gmail.com>
2024-05-09 20:26:56 +0200

    Added SavedJobAds to database to be able to save JobAds to the user

--- Migrations/20240509173822_Adding_SavedJobAds.Designer.cs
+++ Migrations/20240509173822_Adding_SavedJobAds.Designer.cs
@@ -4,6 +4,7 @@ using ChasGPT_Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChasGPT_Backend.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20240509173822_Adding_SavedJobAds")]
+    partial class Adding_SavedJobAds
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
