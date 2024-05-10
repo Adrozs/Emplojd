c73446b1dd4f4a1976b46281c3db3b8b35f12073
Adrozs <adrozsahegyi@gmail.com>
2024-05-10 13:00:39 +0200

    Added data annotations to the dto to test ModelState fo automatic validation of api body input but it doesn't seem to work with the minimal api setup. still testing. Doesn't seem to harm keeping this here for now.

--- ViewModels & DTOs/Account/CreateAccountRequestDto.cs
+++ ViewModels & DTOs/Account/CreateAccountRequestDto.cs
@@ -1,10 +1,23 @@
-﻿namespace ChasGPT_Backend.ViewModels___DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChasGPT_Backend.ViewModels___DTOs
 {
     public class CreateAccountRequestDto
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string EmailConfirmed { get; set; }
+
+        [Required]
+        [StringLength(256, MinimumLength = 8)]
         public string Password { get; set; }
+
+        [Required]
+        [StringLength(256, MinimumLength = 8)]
         public string PasswordConfirmed { get; set; }
     }
 }
