7766ff6c5df2314b22b12e88ec2a55954b647120
Adrozs <adrozsahegyi@gmail.com>
2024-05-12 12:01:21 +0200

    Added attributes to all dtos. Makes it so that programs like swagger understands what it is meant to be, and it helps with error handling. If we in the future switch to a full API instead of minimal API we can make use of the "ModelState" for built in validation.

--- ViewModels & DTOs/Account/ChangePasswordRequestDto.cs
+++ ViewModels & DTOs/Account/ChangePasswordRequestDto.cs
@@ -1,9 +1,22 @@
-﻿namespace ChasGPT_Backend.ViewModels___DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChasGPT_Backend.ViewModels___DTOs
 {
     public class ChangePasswordRequestDto
     {
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        [StringLength(256, MinimumLength = 8)]
         public string CurrentPassword { get; set; }
+
+        [Required(ErrorMessage = "New password is required.")]
+        [DataType(DataType.Password)]
+        [StringLength(256, MinimumLength = 8)]
         public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "New password confirmation is required.")]
+        [DataType(DataType.Password)]
+        [StringLength(256, MinimumLength = 8)]
         public string NewPasswordConfirm { get; set; }
     }
 }
--- ViewModels & DTOs/Account/CreateAccountRequestDto.cs
+++ ViewModels & DTOs/Account/CreateAccountRequestDto.cs
@@ -1,22 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ChasGPT_Backend.ViewModels___DTOs
 {
     public class CreateAccountRequestDto
     {
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "Email address is required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
         public string Email { get; set; }
 
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "Email address confirmation is required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
         public string EmailConfirmed { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
         [StringLength(256, MinimumLength = 8)]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Password confirmation is required.")]
+        [DataType(DataType.Password)]
         [StringLength(256, MinimumLength = 8)]
         public string PasswordConfirmed { get; set; }
     }
