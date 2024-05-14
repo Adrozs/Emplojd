4d96368a815397892ad1aa2702cf43e11bb0bfb5
Adrozs <adrozsahegyi@gmail.com>
2024-05-14 08:51:22 +0200

    Fixed comment with the wrong name

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -7,7 +7,6 @@ namespace ChasGPT_Backend.Data
     public class ApplicationContext : IdentityDbContext<User>
     {
         public DbSet<User> Users { get; set; }
-
         public DbSet<JobAd> JobAd { get; set; }
 
 
@@ -18,7 +17,7 @@ namespace ChasGPT_Backend.Data
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
 
 
-        // User and SavedJobAds relationship
+        // User and JobAds relationship
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
