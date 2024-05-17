7cb58781c8715ae8d76abada0b90e3797ec774c8
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-05-17 13:02:45 +0200

    Refactored the Database Logic to the new project

---
+++ Emplojd/Emplojd.Server/Data/ApplicationContext.cs
@@ -0,0 +1,32 @@
+using ChasGPT_Backend.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChasGPT_Backend.Data
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<JobAd> JobAd { get; set; }
+
+
+        public DbSet<CoverLetter> CoverLetters { get; set; }
+        public DbSet<SavedJobAds> SavedJobAds { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+
+        // User and SavedJobAds relationship
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasMany(p => p.SavedJobAds)
+                .WithMany(j => j.Users)
+                .UsingEntity(j => j.ToTable("UserJobAds"));
+        }
+    }
+}
---
+++ Emplojd/Emplojd.Server/ViewModels & DTOs/JobAds/JobSearchResultWrapper.cs
@@ -0,0 +1,87 @@
+﻿using System.Reflection.Emit;
+using System.Text.Json.Serialization;
+
+namespace ChasGPT_Backend.ViewModels
+{
+    public class JobSearchResultWrapper
+    {
+        //public int SearchResults { get; set; } avvakta med detta?
+        [JsonPropertyName("hits")]
+        public List<JobDto> Jobs { get; set; }
+    }
+
+    public class JobDto
+    {
+        public string Id { get; set; }
+        public string Headline { get; set; } 
+        public string Logo_Url { get; set; }
+        //public string ApplicationDeadline { get; set; } 
+        public DescriptionDto Description { get; set; } 
+        public EmploymentTypeDto EmploymentType { get; set; } 
+        //public SalaryTypeDto SalaryType { get; set; } 
+        //public DurationDto Duration { get; set; } 
+        public WorkingHoursTypeDto WorkingHoursType { get; set; } //Heltid
+        public EmployerDto Employer { get; set; } // Rekryterare??
+        public ApplicationDetailsDto ApplicationDetails { get; set; }
+        public OccupationDto Occupation { get; set; }
+        //public List<ApplicationContactDto> ApplicationContacts { get; set; }
+        //public string PublicationDate { get; set; }
+    }
+    public class DescriptionDto
+    {
+        public string Text { get; set; }
+        public string Text_formatted { get; set; }
+    }
+
+    public class EmploymentTypeDto
+    {
+        public string Label { get; set; }
+    }
+
+    //public class SalaryTypeDto
+    //{
+    //    public string Label { get; set; }
+    //    public string Description { get; set; }
+    //}
+
+    //public class DurationDto
+    //{
+    //    public string Label { get; set; }
+    //}
+
+    public class WorkingHoursTypeDto
+    {
+        public string Label { get; set; }
+    }
+
+    public class EmployerDto
+    {
+        public string Name { get; set; }
+    }
+
+    public class ApplicationDetailsDto
+    {
+        public string Reference { get; set; }
+        public string Email { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class OccupationDto
+    {
+        public string Label { get; set; }
+    }
+
+    public class WorkplaceAddressDto
+    {
+        public string Municipality { get; set; }
+    }
+
+    //public class ApplicationContactDto
+    //{
+    //    public string Name { get; set; }
+    //    public string Description { get; set; }
+    //    public string Email { get; set; }
+    //    public string Telephone { get; set; }
+    //}
+
+}
