711c63f55acc4287977205c07e4451a70378e4e5
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-05-17 13:04:17 +0200

    Refactored the Options, Services and Program.cs to the new project.

--- Emplojd/Emplojd.Server/Program.cs
+++ Emplojd/Emplojd.Server/Program.cs
@@ -1,30 +1,231 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+using ChasGPT_Backend.Services;
+using OpenAI_API;
+using ChasGPT_Backend.Models;
+using ChasGPT_Backend.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ChasGPT_Backend.Repository;
+using ChasGPT_Backend.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using ChasGPT_Backend.Helpers;
+using ChasGPT_Backend.Options;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace ChasGPT_Backend
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = GoogleDefaults.AuthenticationScheme;
+            })
+                .AddCookie()
+                .AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
+                {
+                    options.ClientId = builder.Configuration.GetSection("GoogleKeys:ClientId").Value;
+                    options.ClientSecret = builder.Configuration.GetSection("GoogleKeys:ClientSecret").Value;
+                    options.CallbackPath = "/googleresponse";
+                });
+
+
+            ConfigurationManager configuration = builder.Configuration;
+
+            // Add services to the container.
+
+            DotNetEnv.Env.Load();
+
+            // Setup database context and connection string here
+            builder.Services.AddDbContext<ApplicationContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Add CORS services
+            builder.Services.AddCors();
+
+
+            // Adding Microsoft identity with config settings
+            builder.Services.AddIdentity<User, IdentityRole>(options =>
+            {
+                // Password requirements
+                options.Password.RequiredLength = 8;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireDigit = true;
+                options.Password.RequireNonAlphanumeric = true;
+
+                // Ensure email is confirmed
+                options.SignIn.RequireConfirmedEmail = true;
+
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+                options.Lockout.MaxFailedAccessAttempts = 8;
+            })
+            .AddEntityFrameworkStores<ApplicationContext>() // Connects identity to the database giving its method ability to access it
+            .AddDefaultTokenProviders();
+
+            // Add Mailkit email config
+            builder.Services.Configure<MailKitSettings>(configuration.GetSection("MailKitSettings"));
+
+
+            // Adding authentication
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                // Add JWT Bearer
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true; // Allows the server to save the token for the duration of the request
+                    options.RequireHttpsMetadata = false; // Enforces HTTPS so tokens aren't transfered over unsecure connections (THIS IS SET TO FALSE TEMPORARILY DURING PRODUCTION FOR TESTING PURPOSES)
+                    options.TokenValidationParameters = new TokenValidationParameters // The rules of which authorization will check
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = configuration["Jwt:Issuer"],
+                        ValidAudience = configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration["Jwt:Secret"])) // Symetric key lets the system know the same secret is used for both signing and verifying the JWT. Then encodes it into bytes
+                    };
+                });
+
+            // Add authorization
+            builder.Services.AddAuthorization();
+
+
+            // Add to scope
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IJobAdRepository, JobAdRepository>();
+            builder.Services.AddSingleton(provider =>
+                new JwtRepository(provider.GetRequiredService<IConfiguration>()));
+            builder.Services.AddScoped<AuthenticationService>();
+            builder.Services.AddScoped<IEmailSender, EmailSender>();
+            builder.Services.AddSingleton(sp => new OpenAIAPI(Environment.GetEnvironmentVariable("OPENAI_API_KEY")));
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Configure Swagger to include JWT authorization input
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+                // Define the Bearer Authentication scheme
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization", // Name of the header
+                    In = ParameterLocation.Header, // Location of the header
+                    Type = SecuritySchemeType.Http, // Type of the security
+                    Scheme = "bearer" // Scheme name
+                });
+
+                // Make sure Swagger UI requires a Bearer token to be specified
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer" // Must match the scheme name defined in AddSecurityDefinition
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+
+            var app = builder.Build();
+
+            app.UseRouting();
+
+            // Add CORS (CHANGE BEFORE PRODUCTION - ONLY FOR TESTING!) Right now it allows access to any and all
+            app.UseCors(builder =>
+            {
+                builder
+                      .WithOrigins("https://localhost:54686", "http://localhost:54687", "http://localhost:5173", "https://localhost:5173")
+                      .SetIsOriginAllowedToAllowWildcardSubdomains()
+                      .AllowAnyHeader()
+                      .AllowCredentials()
+                      .WithMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
+            });
+
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+
+            //app.UseHttpsRedirection(); // (THIS IS SET TO FALSE TEMPORARILY DURING PRODUCTION FOR TESTING PURPOSES)
+
+
+            // Apply the CORS policy
+            app.UseCors("OpenCorsPolicy");
+
+
+            // Forces all api calls to use the JWT (token) to be authorized. (Unless specified).
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllers();
+
+
+
+
+            // ENDPOINTS
+            // Note: Don't forget to add ".RequireAuthorization()" to your endpoints! Without it you can access them without the token.
+
+            // User account
+            // .AllowAnonymous() to explicitly say that this doesn't require token auth
+            app.MapPost("/login", UserService.LoginAsync).AllowAnonymous();
+            app.MapPost("/create-account", UserService.CreateAccountAsync).AllowAnonymous();
+            app.MapGet("/confirm-email", UserService.EmailVerificationAsync).AllowAnonymous();
+            app.MapPost("/forgot-password", UserService.GeneratePasswordResetTokenAsync).AllowAnonymous();
+            app.MapPost("/reset-password", UserService.ResetPasswordAsync).AllowAnonymous();
+
+            // Is this even necessary anymore when we have /reset-password ??? - only difference with this one is it changed password without email verification
+            //app.MapPost("/change-password", UserService.ChangePasswordAsync).RequireAuthorization();
+
+
+            // Cover letter
+            app.MapGet("/GetPersonalLetter/{userId}/{jobId}/{temperature}/{job}", ChatGPTService.GenerateLetterAsync);
+
+
+            // JobAd search
+            // Made the URI flexible to be able to omit parameters that aren't search from the query
+            app.MapGet("/search", JobAdService.SearchJob).RequireAuthorization();
+            app.MapGet("/ad/{adId}", JobAdService.GetJobFromId).RequireAuthorization();
+
+
+            // JobAds Get/Save/Delete
+            app.MapGet("/saved-ads", JobAdService.GetSavedJobAdsAsync).RequireAuthorization();
+            app.MapPost("/save-ad", JobAdService.SaveJobAdAsync).RequireAuthorization();
+            app.MapDelete("/saved-ad", JobAdService.RemoveSavedJobAdAsync).RequireAuthorization();
+
+
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapFallbackToFile("/index.html");
-
-app.Run();
---
+++ Emplojd/Emplojd.Server/Services/JobAdService.cs
@@ -0,0 +1,124 @@
+﻿using ChasGPT_Backend.Helpers;
+using ChasGPT_Backend.Repositories;
+using ChasGPT_Backend.ViewModels;
+using ChasGPT_Backend.ViewModels___DTOs.JobAds;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace ChasGPT_Backend.Services
+{
+    public class JobAdService
+    {
+        public static async Task<IResult> SearchJob([FromServices] IJobAdRepository jobAdRepository, [FromQuery] string search, [FromQuery] int? region, [FromQuery] int page = 1)
+        {
+            try
+            {
+                List<JobDto> result = await jobAdRepository.GetJobAdsAsync(search, region, page);
+
+                if (result.Count == 0)
+                    return Results.NotFound("No job ads matching search found.");
+
+                return Results.Json(result);
+            }
+            catch (Exception ex)
+            {
+                return ExceptionHandler.HandleException(ex);
+            }
+        }
+
+        public static async Task<IResult> GetJobFromId([FromQuery] int adId, [FromServices] IJobAdRepository jobAdRepository)
+        {
+            try
+            {
+                JobDto result = await jobAdRepository.GetJobAdFromIdAsync(adId);
+
+                if (result == null)
+                    return Results.NotFound($"No job ad with id \"{adId}\" was found");
+       
+               return Results.Json(result);
+                
+            }
+            catch (Exception ex)
+            {
+                return ExceptionHandler.HandleException(ex);
+            }
+        }
+        
+
+        public static async Task<JobChatGptDto> GetJobAdFromIdChatGpt(int jobId, IJobAdRepository jobAdRepository)
+        {
+            try
+            {
+                // Returns the JobAd if found or null if no matching ad was found
+                return await jobAdRepository.GetJobAdFromIdChatGptAsync(jobId);
+            }
+            catch (Exception)
+            {
+                // If an exception occurred rethrow the error to the method calling it for it to handle.
+                throw;
+            }
+        }
+
+
+        public static async Task<IResult> GetSavedJobAdsAsync(ClaimsPrincipal currentUser, [FromServices] IJobAdRepository jobAdRepository)
+        {
+            try
+            {
+                List<SavedJobAdDto> jobAds = await jobAdRepository.GetSavedJobAdsAsync(currentUser);
+
+                if (jobAds.Count == 0)
+                    return Results.NotFound("No saved job ads were found.");
+
+                return Results.Json(jobAds);
+
+            }
+            catch (Exception ex)
+            {
+                return ExceptionHandler.HandleException(ex);
+            }
+        }
+
+        public static async Task<IResult> SaveJobAdAsync([FromBody] SaveJobAdRequest request, ClaimsPrincipal currentUser, [FromServices] IJobAdRepository jobAdRepository)
+        {
+            if (request.PlatsbankenJobAdId == 0 || string.IsNullOrEmpty(request.Headline) || string.IsNullOrEmpty(request.Employer))
+                return Results.BadRequest("Invalid request data. All job ad fields must be filled out.");
+
+            try
+            {
+                bool result = await jobAdRepository.SaveJobAdAsync(request, currentUser);
+
+                if (!result)
+                    return Results.Problem("User has already saved this job ad.");
+
+                return Results.Ok("Job ad successfully saved.");
+
+            }
+            catch (Exception ex)
+            {
+                return ExceptionHandler.HandleException(ex);
+            }
+        }
+
+        public static async Task<IResult> RemoveSavedJobAdAsync([FromBody] RemoveJobAdRequest request, ClaimsPrincipal currentUser, [FromServices] IJobAdRepository jobAdRepository)
+        {
+            if (request.PlatsbankenJobAdId == 0)
+                return Results.BadRequest("Invalid request data. All job ad fields must be filled out.");
+
+            try
+            {
+                bool result = await jobAdRepository.RemoveSavedJobAdAsync(request.PlatsbankenJobAdId, currentUser);
+
+                if (!result)
+                    return Results.Problem("User has not saved this job ad.");
+
+                return Results.Ok("Job ad successfully removed.");
+
+            }
+            catch (Exception ex)
+            {
+                return ExceptionHandler.HandleException(ex);
+            }
+        }
+    }
+}
