a3637bd7fbd23a511bfaf326009ff0fa4ba37170
Simon-jmk <151679315+Simon-jmk@users.noreply.github.com>
2024-05-18 04:01:55 +0200

    Footer and header wave

--- src/components/Header/HeaderOtherPages.jsx
+++ src/components/Header/HeaderOtherPages.jsx
@@ -4,70 +4,6 @@ import { RxHamburgerMenu } from "react-icons/rx";
 
 import Modal from "../Modal";
 
-const DynamicSVG = () => {
-  const strokeWidth = `calc(100vh / 9)`;
-
-  return (
-    <svg viewBox="0 0 360 94" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <g filter="url(#filter0_d_989_1081)">
-        <path
-          d="M377 12.0652L335.946 15.7609L288.291 20.7015C260.831 23.5485 233.091 21.9408 206.143 15.9407L183.37 10.8699C124.604 -2.2148 63.6725 -2.14683 4.93623 11.069V11.069C1.99347 11.7311 -1.01358 12.0652 -4.02991 12.0652H-6.00001"
-          stroke="url(#paint0_linear_989_1081)"
-          style={{ strokeWidth }}
-        />
-      </g>
-      <defs>
-        <filter
-          id="filter0_d_989_1081"
-          x="-22.2468"
-          y="-78.3936"
-          width="429.223"
-          height="171.962"
-          filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
-        >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
-          <feColorMatrix
-            in="SourceAlpha"
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-            result="hardAlpha"
-          />
-          <feOffset dx="5" dy="-4" />
-          <feGaussianBlur stdDeviation="10" />
-          <feComposite in2="hardAlpha" operator="out" />
-          <feColorMatrix
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0"
-          />
-          <feBlend
-            mode="normal"
-            in2="BackgroundImageFix"
-            result="effect1_dropShadow_989_1081"
-          />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="effect1_dropShadow_989_1081"
-            result="shape"
-          />
-        </filter>
-        <linearGradient
-          id="paint0_linear_989_1081"
-          x1="5.96877"
-          y1="-9"
-          x2="12.3505"
-          y2="58.3957"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop offset="0.25" stop-color="#A78BFA" />
-          <stop offset="1" stop-color="#0EA5E9" />
-        </linearGradient>
-      </defs>
-    </svg>
-  );
-};
-
 const HeaderOtherPages = () => {
   const [menu, setMenu] = useState(false);
 
@@ -76,8 +12,71 @@ const HeaderOtherPages = () => {
   }
   return (
     <header className="flex justify-between w-full h-24 items-start">
-      <div className="flex w-full">
-        <DynamicSVG className="w-full h-full" />
+      <div className="w-full">
+        <svg
+          viewBox="0 0 360 108"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="-top-4 absolute"
+        >
+          <g filter="url(#filter0_d_1273_1292)">
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M208.773 70.8864C233.46 66.2583 258.683 65.1877 283.674 67.7072L328.462 72.2227L350.206 75.0001H376V-11.9999H355.74L338.339 -14.2226L292.401 -18.854C259.149 -22.2063 225.59 -20.7819 192.742 -14.624L173.105 -10.9427C117.714 -0.558667 60.8905 -0.403079 5.4437 -10.4836C-0.105408 -11.4925 -5.73404 -11.9999 -11.3741 -11.9999H-12V75.0001H-11.3741C-10.9529 75.0001 -10.5326 75.038 -10.1183 75.1133C55.7764 87.0933 123.308 86.9084 189.136 74.5677L208.773 70.8864Z"
+              fill="url(#paint0_linear_1273_1292)"
+            />
+          </g>
+          <defs>
+            <filter
+              id="filter0_d_1273_1292"
+              x="-32"
+              y="-36.6204"
+              width="428"
+              height="144.583"
+              filterUnits="userSpaceOnUse"
+              colorInterpolationFilters="sRGB"
+            >
+              <feFlood floodOpacity="0" result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset dy="4" />
+              <feGaussianBlur stdDeviation="10" />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0"
+              />
+              <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_1273_1292"
+              />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_1273_1292"
+                result="shape"
+              />
+            </filter>
+            <linearGradient
+              id="paint0_linear_1273_1292"
+              x1="324.031"
+              y1="-20.6204"
+              x2="290.964"
+              y2="136.714"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop offset="0.145906" stopColor="#A78BFA" />
+              <stop offset="1" stopColor="#0EA5E9" />
+            </linearGradient>
+          </defs>
+        </svg>
+
         <div className="flex absolute w-full pt-2">
           <nav className="flex justify-between w-full items-center">
             <div className="p-2">
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -15,10 +15,7 @@ export default function LandingPage() {
         className="bg-gradient-to-b from-[#CA81ED] to-[#4086C6] bg-cover bg-no-repeat h-screen lg:pb-20"
         id="hero"
       >
-        <Header>
-          {" "}
-          
-        </Header>
+        <Header> </Header>
         <div className=" container flex flex-col-reverse items-center  px-6 mx-auto space-y-0 mt-[-100px] md:space-y-0 flex-grow  md:flex-grow  sm:mt-[-75px] md:mt-[-75px]">
           <div className="flex flex-col items-center justify-center mt-48 space-y-12 md:w-1/2 text-white">
             <h1 className="text-3xl">
@@ -61,6 +58,21 @@ export default function LandingPage() {
       </section>
 
       <section>
+      <div className="w-full relative -top-10">
+          <svg
+            viewBox="0 0 360 81"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M140.276 59.1272C117.122 52.1522 92.7316 50.2797 68.7843 53.6387L21.4314 60.2807L-19.8566 65.3092L-26.1433 13.6907L14.6759 8.7192L61.5612 2.14278C92.9521 -2.26029 124.924 0.194269 155.275 9.33735L178.054 16.1993C231.376 32.2622 288.259 32.1796 341.534 15.962C346.896 14.3296 352.471 13.4999 358.076 13.4999H360V65.4999H358.076C357.602 65.4999 357.131 65.5701 356.677 65.7081C293.58 84.916 226.208 85.0137 163.055 65.9892L140.276 59.1272Z"
+              fill="#eceef0"
+            />
+          </svg>
+        </div>
+
         <div ref={learnMore}></div>
         <div className=" flex justify-center  ">
           <h6 className="my-4 flex-col font-semi-bold items-center justify-center text-center text-2xl font-bold ">
@@ -68,20 +80,7 @@ export default function LandingPage() {
           </h6>
         </div>
 
-        <div className="w-full ">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1440 130"
-            className=" absolute bottom-0 w-full"
-          >
-            <path
-              fill="#eceef0"
-              fillOpacity="1"
-              d="M0,80L80,72C160,64,320,48,480,48C640,48,800,64,960,72C1120,80,1280,80,1360,80L1440,80L1440,160L1360,160C1280,160,1120,160,960,160C800,160,640,160,480,160C320,160,160,160,80,160L0,160Z"
-            ></path>
-          </svg>
-        </div>
-
+        
         <SearchForm />
 
         <ActiveSlider />
@@ -94,7 +93,7 @@ export default function LandingPage() {
           </Link>
         </div>
       </section>
-      <p className="ms-10 mr-10 mt-5 mb-10 md:mb-16">
+      <p className="ms-10 mr-10 mt-5 mb-20 md:mb-16">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
