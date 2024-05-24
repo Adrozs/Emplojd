1a3d4a45b185a2e1648a08be4c1b4a965927367d
Adrozs <adrozsahegyi@gmail.com>
2024-05-24 08:43:14 +0200

    Added result method for returing a result instead of a bool with the coverletter methods

--- Emplojd/Emplojd.Server/Helpers/ResultHandler.cs
+++ Emplojd/Emplojd.Server/Helpers/ResultHandler.cs
@@ -16,5 +16,18 @@ namespace Emplojd.Helpers
                 return Results.BadRequest($"{failMessage} {errors}");
             }
         }
+        
+        public static IResult HandleCoverLetterResult(CoverLetterResult result, string successMessage, string failMessage)
+        {
+            if (result.Success)
+            {
+                return Results.Ok(successMessage);
+            }
+            else
+            {
+                var errors = string.Join(",", result.ErrorMessage);
+                return Results.BadRequest($"{failMessage} {errors}");
+            }
+        }
     }
 }
