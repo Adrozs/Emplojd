25fa8a832484cb9bf88372a265ca6fc3c748d3b0
Fady Hatta <Fadynhatta@gmail.com>
2024-05-25 16:36:10 +0200

    SaveJobAdRequest has now been moved to the new file system

--- ViewModels & DTOs/JobAds/SaveJobAdRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChasGPT_Backend.ViewModels___DTOs.JobAds
-{
-    public class SaveJobAdRequest
-    {
-        public int PlatsbankenJobAdId { get; set; }
-        public string Headline { get; set; }
-        public string Employer { get; set; }
-    }
-}
