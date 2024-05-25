5acf52ea8e7bd7f4746a28f60c9cd55e44693666
Fady Hatta <Fadynhatta@gmail.com>
2024-05-25 16:20:43 +0200

    UserNotFoundException has been deleted from the old project map. There is another within the new file system.

--- Exceptions/JobAdExceptions/UserNotFoundException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ChasGPT_Backend.Exceptions.JobAdExceptions
-{
-    public class UserNotFoundException : ApiException
-    {
-        public override int StatusCode => StatusCodes.Status404NotFound;
-        public UserNotFoundException(string message) : base(message) { }
-    }
-}
