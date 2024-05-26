4120698209a6f27002888d2150e14b6290251ea5
Fady Hatta <Fadynhatta@gmail.com>
2024-05-26 22:24:05 +0200

    Added a new UserProfileController.

---
+++ Emplojd/Emplojd.Server/Controllers/UserProfileController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Emplojd.Server.Services;
+using Emplojd.Server.ViewModels___DTOs;
+using System.Threading.Tasks;
+
+namespace Emplojd.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly UserProfileService _userProfileService;
+
+        public UserProfileController(UserProfileService userProfileService)
+        {
+            _userProfileService = userProfileService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUserProfile([FromBody] UserProfileDto userProfileDto)
+        {
+            await _userProfileService.AddUserProfileAsync(userProfileDto);
+            return Ok();
+        }
+    }
+}
