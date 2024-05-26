4b41315dcd40a4c383052cd72a1fbc2255afb521
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-05-26 23:18:19 +0200

    Renamed properties to match the rest of the naming scheme

--- Emplojd/Emplojd.Server/Models/SavedJobAd.cs
+++ Emplojd/Emplojd.Server/Models/SavedJobAd.cs
@@ -4,12 +4,11 @@ namespace Emplojd.Models
 {
     public class SavedJobAd
     {
-        [Key]
         public int SavedJobAdId { get; set; }
-        public int PlatsbankenJobId { get; set; }
+        public int PlatsbankenJobAdId { get; set; }
         public string Headline { get; set; }  
         public string Employer { get; set; } 
 
-        public virtual ICollection<User> User { get; set; }
+        public virtual ICollection<User> Users { get; set; }
     }
 }
