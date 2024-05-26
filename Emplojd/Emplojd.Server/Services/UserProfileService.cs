a6894a7b180b857dc8741ef3ff1c9dd720faed2d
Fady Hatta <Fadynhatta@gmail.com>
2024-05-26 23:16:50 +0200

    Added two more methods; as we've already made two methods that handles POSTing to the DB; Now we have two that GETs methods from the DB; Will update the Controller accordingly.

--- Emplojd/Emplojd.Server/Services/UserProfileService.cs
+++ Emplojd/Emplojd.Server/Services/UserProfileService.cs
@@ -2,6 +2,7 @@
 using Emplojd.Models;
 using Emplojd.Server.Models;
 using Emplojd.Server.ViewModels___DTOs;
+using Emplojd.Server.ViewModels___DTOs.UserProfile;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,29 +20,17 @@ namespace Emplojd.Server.Services
 
         public async Task AddUserProfileAsync(UserProfileDto userProfileDto)
         {
-            var user = await _context.Users
-                .Include(u => u.CvManually)
-                .FirstOrDefaultAsync(u => u.Id == userProfileDto.Id);
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userProfileDto.Id);
 
             if (user == null)
             {
                 user = new User
                 {
                     Id = userProfileDto.Id,
-                    UserName = userProfileDto.Id, // IF we assume Id is the same as UserName
                     Name = userProfileDto.Name,
                     UserInterestTags = userProfileDto.UserInterestTags,
                     DescriptiveWords = userProfileDto.DescriptiveWords,
                     CvContentText = userProfileDto.CvContentText,
-                    CvManually = userProfileDto.CvManually?.Select(c => new CvManually
-                    {
-                        CvManuallyId = c.CvManuallyId,
-                        PositionEducation = c.PositionEducation,
-                        StartDate = c.StartDate,
-                        EndDate = c.EndDate,
-                        SchoolWorkplace = c.SchoolWorkplace,
-                        IsEducation = c.IsEducation,
-                    }).ToList()
                 };
 
                 _context.Users.Add(user);
@@ -52,9 +41,19 @@ namespace Emplojd.Server.Services
                 user.UserInterestTags = userProfileDto.UserInterestTags;
                 user.DescriptiveWords = userProfileDto.DescriptiveWords;
                 user.CvContentText = userProfileDto.CvContentText;
+            }
 
-                // Update CvManually entries
-                user.CvManually = userProfileDto.CvManually?.Select(c => new CvManually
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AddUserCvManuallyAsync(int userId, List<CvManuallyDto> cvManuallyDtos)
+        {
+            var userIdString = userId.ToString();
+            var user = await _context.Users.Include(u => u.CvManually).FirstOrDefaultAsync(u => u.Id == userIdString);
+
+            if (user != null)
+            {
+                user.CvManually = cvManuallyDtos.Select(c => new CvManually
                 {
                     CvManuallyId = c.CvManuallyId,
                     PositionEducation = c.PositionEducation,
@@ -63,9 +62,48 @@ namespace Emplojd.Server.Services
                     SchoolWorkplace = c.SchoolWorkplace,
                     IsEducation = c.IsEducation,
                 }).ToList();
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<UserProfileDto> GetUserProfileAsync(string userId)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+
+            if (user == null)
+            {
+                return null;
             }
 
-            await _context.SaveChangesAsync();
+            return new UserProfileDto
+            {
+                Id = user.Id,
+                Name = user.Name,
+                UserInterestTags = user.UserInterestTags,
+                DescriptiveWords = user.DescriptiveWords,
+                CvContentText = user.CvContentText
+            };
+        }
+
+        public async Task<List<CvManuallyDto>> GetUserCvManuallyAsync(string userId)
+        {
+            var user = await _context.Users.Include(u => u.CvManually).FirstOrDefaultAsync(u => u.Id == userId);
+
+            if (user == null || user.CvManually == null)
+            {
+                return new List<CvManuallyDto>(); // better handling if we get an empty list instead of null as it wont conflict with the CV generation
+            }
+
+            return user.CvManually.Select(c => new CvManuallyDto
+            {
+                CvManuallyId = c.CvManuallyId,
+                PositionEducation = c.PositionEducation,
+                StartDate = c.StartDate,
+                EndDate = c.EndDate,
+                SchoolWorkplace = c.SchoolWorkplace,
+                IsEducation = c.IsEducation
+            }).ToList();
         }
     }
 }
