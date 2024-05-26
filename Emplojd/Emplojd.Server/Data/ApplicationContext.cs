c5c4626a98d7d3caf6fe3b641260cd3b441a830d
Fady Hatta <Fadynhatta@gmail.com>
2024-05-26 22:24:19 +0200

    Updated the ApplicationContext accordingly.

--- Emplojd/Emplojd.Server/Data/ApplicationContext.cs
+++ Emplojd/Emplojd.Server/Data/ApplicationContext.cs
@@ -7,26 +7,31 @@ namespace Emplojd.Data
 {
     public class ApplicationContext : IdentityDbContext<User>
     {
-        public DbSet<User> Users { get; set; }
-
-        public DbSet<JobAd> JobAd { get; set; }
-
-
-        public DbSet<SavedCoverLetter> CoverLetters { get; set; }
-        public DbSet<SavedJobAd> SavedJobAds { get; set; }
-
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
 
+        public DbSet<User> Users { get; set; }
+        public DbSet<JobAd> JobAd { get; set; }
+        public DbSet<SavedCoverLetter> CoverLetters { get; set; }
+        public DbSet<SavedJobAd> SavedJobAds { get; set; }
+        public DbSet<CvManually> CvManuallys { get; set; }
 
-        // User and SavedJobAds relationship
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            // User and SavedJobAds relationship
             modelBuilder.Entity<User>()
                 .HasMany(p => p.SavedJobAds)
                 .WithMany(j => j.User)
                 .UsingEntity(j => j.ToTable("UserJobAds"));
+
+            // Configure the User and CvManually relationship
+            modelBuilder.Entity<CvManually>()
+                .HasOne(cm => cm.User)
+                .WithMany(u => u.CvManually)
+                .HasForeignKey(cm => cm.UserId);
+
+            // Additional configurations can be added here
         }
     }
 }
