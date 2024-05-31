f9e3ff1ec35220a6f24d73d7d1bff4b32ce6f64c
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-05-31 18:41:14 +0200

    Added company name and date to cover letter model

--- Emplojd/Emplojd.Server/Migrations/20240531162220_AddingInfoToCoverLetter.Designer.cs
+++ Emplojd/Emplojd.Server/Migrations/20240531162220_AddingInfoToCoverLetter.Designer.cs
@@ -4,6 +4,7 @@ using Emplojd.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Emplojd.Server.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20240531162220_AddingInfoToCoverLetter")]
+    partial class AddingInfoToCoverLetter
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
