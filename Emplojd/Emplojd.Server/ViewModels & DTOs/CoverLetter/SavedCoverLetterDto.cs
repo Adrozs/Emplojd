931c21c02fb427c7151a3985008f5956ef5766fb
Adrian Rozsahegyi <122897788+Adrozs@users.noreply.github.com>
2024-05-31 18:42:17 +0200

    Added company name and date parameters to dtos

--- Emplojd/Emplojd.Server/ViewModels & DTOs/CoverLetter/SavedCoverLetterDto.cs
+++ Emplojd/Emplojd.Server/ViewModels & DTOs/CoverLetter/SavedCoverLetterDto.cs
@@ -6,5 +6,7 @@
         public float Temperature { get; set; }
         public string CoverLetterTitle { get; set; }
         public string CoverLetterContent{ get; set; }
+        public string CompanyName { get; set; }
+        public DateTime Date { get; set; }
     }
 }
