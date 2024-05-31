9117b3effb75ebbf0ad7650cbd1865091847b1b6
Fady Hatta <Fadynhatta@gmail.com>
2024-05-31 16:57:51 +0200

    Added our new Domain to Cors

--- Emplojd/Emplojd.Server/Program.cs
+++ Emplojd/Emplojd.Server/Program.cs
@@ -219,7 +219,7 @@ namespace Emplojd
             app.UseCors(builder =>
             {
                 builder
-                      .WithOrigins("https://localhost:54686", "http://localhost:54687", "http://localhost:5173", "https://localhost:5173", "https://localhost:4173")
+                      .WithOrigins("https://localhost:54686", "http://localhost:54687", "http://localhost:5173", "https://localhost:5173", "https://localhost:4173", "https://www.emplojd.com")
                       .SetIsOriginAllowedToAllowWildcardSubdomains()
                       .AllowAnyHeader()
                       .AllowCredentials()
