f6d418ca13e347e5e040f3f8f13c8b7e5ec5fee2
Adrozs <adrozsahegyi@gmail.com>
2024-06-02 19:22:21 +0200

    Added request dtos for each method to be able to set specific properties and values for separare methods

---
+++ Emplojd/Emplojd.Server/ViewModels & DTOs/UserProfile/SaveCvManuallyRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Emplojd.Server.ViewModels___DTOs.UserProfile
+{
+    public class SaveCvManuallyRequest
+    {
+        public int? CvManuallyId { get; set; } = null;
+        public string PositionEducation { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string SchoolWorkplace { get; set; }
+        public bool IsEducation { get; set; }
+
+    }
+}
