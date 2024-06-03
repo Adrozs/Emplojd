12925a6e0f7eb25e0fcedb1dad2d96f8ff9c225a
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-06-03 17:31:50 +0200

    Created a Dto for the ResumeFilePath

---
+++ Emplojd/Emplojd.Server/ViewModels & DTOs/UserProfile/GetResumeDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Emplojd.Server.ViewModels___DTOs.UserProfile
+{
+    public class GetResumeDto
+    {
+        [JsonPropertyName("resumeFilePath")]
+        public string? ResumeFilePath { get; set; }
+    }
+}
