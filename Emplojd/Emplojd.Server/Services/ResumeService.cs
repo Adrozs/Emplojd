4df314a531956e5d3db3fc100c4ad03aa0c522b1
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-06-03 13:58:54 +0200

    Updated the ResumeService with access to the User model

--- Emplojd/Emplojd.Server/Services/ResumeService.cs
+++ Emplojd/Emplojd.Server/Services/ResumeService.cs
@@ -1,21 +1,33 @@
-﻿using Emplojd.Server.ViewModels___DTOs.UserProfile;
+﻿using Emplojd.Data;
+using Emplojd.Server.ViewModels___DTOs.UserProfile;
+using System.Security.Claims;
 
 namespace Emplojd.Server.Services
 {
     public class ResumeService
     {
         private readonly BlobStorageService _blobStorageService;
+        private readonly ApplicationContext _context;
 
-        public ResumeService(BlobStorageService blobStorageService)
+        public ResumeService(BlobStorageService blobStorageService, ApplicationContext context)
         {
             _blobStorageService = blobStorageService;
+            _context = context;
         }
 
-        public async Task<string?> StoreResumeAsync(ResumeDto resumeDto)
+        public async Task<string?> StoreResumeAsync(ResumeDto resumeDto, ClaimsPrincipal currentUser)
         {
             if (resumeDto.ResumeFile == null)
             {
                 throw new ArgumentNullException(nameof(resumeDto.ResumeFile), "Resume file cannot be null.");
+            }
+            var email = currentUser.FindFirst(ClaimTypes.Email)?.Value;
+
+            var user = _context.Users.SingleOrDefault(u => u.Email == email);
+
+            if (user == null)
+            {
+                throw new ArgumentException("User not found.");
             }
 
             string fileExtension = Path.GetExtension(resumeDto.ResumeFile.FileName);
@@ -40,6 +52,8 @@ namespace Emplojd.Server.Services
             // Update the ResumeFilePath property in the ResumeDto
             resumeDto.ResumeFilePath = filePath;
 
+            user.ResumeFilePath = filePath;
+            await _context.SaveChangesAsync();
             return filePath;
         }
     }
