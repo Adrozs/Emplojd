0311711d70b4117c7c5f264ef96fc3aa7d2534f4
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-06-03 15:38:38 +0200

    Added the ResumePath migrations

---
+++ Emplojd/Emplojd.Server/Migrations/20240603131637_ResumePath.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Emplojd.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ResumePath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResumeFilePath",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserResume",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResumeFilePath",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserResume",
+                table: "AspNetUsers");
+        }
+    }
+}
