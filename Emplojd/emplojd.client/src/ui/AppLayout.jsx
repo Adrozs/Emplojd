ab7dcbba152064d524b8300076b6019a149ff498
Andy <andycheung83a@hotmail.com>
2024-06-03 21:57:09 +0200

    fix: Darkmode bug in AppLayout.jsx

--- Emplojd/emplojd.client/src/ui/AppLayout.jsx
+++ Emplojd/emplojd.client/src/ui/AppLayout.jsx
@@ -1,17 +1,17 @@
-import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "./Loader";
+import { Outlet, useNavigation } from 'react-router-dom';
+import Loader from './Loader';
 function AppLayout() {
-  const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+	const navigation = useNavigation();
+	const isLoading = navigation.state === 'loading';
 
-  return (
-    <div className="layout bg-[#ECEEF0]">
-      {isLoading && <Loader />}
-      <main>
-        <Outlet />
-      </main>
-    </div>
-  );
+	return (
+		<div className="layout bg-[#ECEEF0] dark:bg-gray-800">
+			{isLoading && <Loader />}
+			<main>
+				<Outlet />
+			</main>
+		</div>
+	);
 }
 
 export default AppLayout;
