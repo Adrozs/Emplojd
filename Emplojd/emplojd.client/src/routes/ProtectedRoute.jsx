ac2d3bbe3830465ba0674b98db1ea899d7a9031a
Andy <andycheung83a@hotmail.com>
2024-06-03 17:24:12 +0200

    Fix: Fixed Problem with Routing and Auth.
    When logged in and manually write /myprofile in the url, it rederected you to the sign in page as if you we're logged out.

--- Emplojd/emplojd.client/src/routes/ProtectedRoute.jsx
+++ Emplojd/emplojd.client/src/routes/ProtectedRoute.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
-    const { isLoggedIn } = useAuth();
+	const authToken = localStorage.getItem('authToken');
 
-    return isLoggedIn ? children : <Navigate to="/signin" replace />;
+	return authToken ? children : <Navigate to="/signin" replace />;
 };
 
 export default ProtectedRoute;
+
+// import React from 'react';
+// import { Navigate } from 'react-router-dom';
+// import { useAuth } from '../context/AuthContext';
+
+// const ProtectedRoute = ({ children }) => {
+// 	const { isLoggedIn } = useAuth();
+
+// 	return isLoggedIn ? children : <Navigate to="/signin" replace />;
+// };
+
+// export default ProtectedRoute;
+// import React from 'react';
+// import { Navigate } from 'react-router-dom';
+// import { useAuth } from '../context/AuthContext';
+
+// const ProtectedRoute = ({ children }) => {
+//     const { isLoggedIn } = useAuth();
+
+//     return isLoggedIn ? children : <Navigate to="/signin" replace />;
+// };
+
+// export default ProtectedRoute;
